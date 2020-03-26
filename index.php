fcb72fc36f35967af723103dd865386b4a44b3dd
Kingston <kingston@vanlaak.cc>
2020-03-26 12:30:15 +0100

    last

--- index.php
+++ index.php
@@ -22,18 +22,18 @@ $taken = $query0072->fetchAll();
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="css/index.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     </head>
     <body>
 
     <div id="myDIV" class="header">
-        <h2 style="margin:5px">My To Do List</h2>
-        <input type="text" id="myInput" placeholder="Title...">
-        <span onclick="newElement()" class="addBtn">Add</span>
+        <h2 style="margin:5px">Taken lijst</h2>
+        <a href='create.php'><span class="addBtn">Toevoegen</span></a>
     </div>
 
     <ul id="myUL">
         <?php foreach($taken as $task){ ?>
-            <li><?php echo $task['title']; ?></li>
+            <li><?php echo $task['title'] . '<br>' . '<div id="beschrijving">' . $task['beschrijving'] . '</div>'; ?></li>
         <?php } ?>
     </ul>
     
