aba4d6cda5bd5f3f4b3dece40f3d7b90363b4c71
Kingston <kingston@vanlaak.cc>
2020-06-09 14:03:54 +0200

    delete proces

--- deleteProc.php
+++ deleteProc.php
@@ -4,7 +4,7 @@ require 'conn.php';
 $id = $_GET['id'];
 
 if(isset($id)){
-    $schaatje2 = $conn->prepare('DELETE FROM taken WHERE id=:idtjes');
+    $schaatje2 = $conn->prepare('DELETE FROM onderwerpen WHERE id=:idtjes');
     $schaatje2->execute(array(':idtjes' => $id));
 
     header('Location:index.php');
--- index.php
+++ index.php
@@ -42,7 +42,7 @@ $taken = $query0072->fetchAll();
             ?>
             <li>
             <a href='edit.php?id=<?php echo $task['id']; ?>' style='position:absolute; right:40px;' class='edit'>Edit</a>
-            <?php echo '<h3>'.$task['title'].'</h3>' . '</a>' . 'Taken:'; foreach($onderwerpen as $ond){ echo '<div style='; if($ond['statuss'] == 'check'){ echo 'color:green'; } echo '>' . $ond['taak'] . " <a href='stat.php?id={$ond['id']} '> ✔</a> <a href='edit.php?id={$ond['id']} '> ✏️</a>" . '</div>'; } ?>
+            <?php echo '<h3>'.$task['title'].'</h3>' . '</a>' . 'Taken:'; foreach($onderwerpen as $ond){ echo '<div style='; if($ond['statuss'] == 'check'){ echo 'color:green'; } echo '>' . $ond['taak'] . " <a href='stat.php?id={$ond['id']} '> ✔</a> <a href='edit.php?id={$ond['id']} '> ✏️</a> <a href='deleteProc.php?id={$ond['id']} '> ✗</a>" . '</div>'; } ?>
             <a href='deleteProc.php?id=<?php echo $task['id']; ?>'>
             <span class='close'>x</span></li>
             </a>
