d22de1775bd033417e2a0dff335eb45e144803db
Kingston <kingston@vanlaak.cc>
2020-06-17 10:58:35 +0200

    laaste update

--- createProc.php
+++ createProc.php
@@ -10,7 +10,7 @@ $taken = $_POST['taken'];
 /**
  * kijkt of het allemaal wel gezet is
  */
-if(isset($title) && isset($taken)){
+if(isset($title) && isset($taken) && !isset($_POST['idTask'])){
     $query = $conn->prepare('INSERT INTO taken(title) VALUES(:title)');
     $query->execute(array(':title' => $title));
 
@@ -21,10 +21,16 @@ if(isset($title) && isset($taken)){
         $query = $conn->prepare('INSERT INTO onderwerpen(taak_id, taak) VALUES(:id, :taak)');
         $query->execute(array(':id' => $last_id, ':taak' => $taak));
     }
-
-    header('Location:index.php');
-} else {
-    header('Location:index.php');
 }
+
+if(isset($taken) && isset($_POST['idTask'])){
+
+    foreach($taken as $taak){
+        $query = $conn->prepare('INSERT INTO onderwerpen(taak_id, taak) VALUES(:id, :taak)');
+        $query->execute(array(':id' => $_POST['idTask'], ':taak' => $taak));
+    }
+}
+
+header('Location:index.php');
 
 ?>
