d2e69063f540ff827f06566b5d7deb5bda1214b7
Kingston <kingston@vanlaak.cc>
2020-06-23 14:49:51 +0200

    Update index.php
    laatste filter

--- index.php
+++ index.php
@@ -25,15 +25,26 @@ $taken = $query0072->fetchAll();
         <h2 style="margin:5px">Taken lijst</h2>
         <a href='create.php'><span class="addBtn">Toevoegen</span></a>
         <a href='index.php?filter=datum'><span  style='margin-left:1em;margin-right:1em;' class="addBtn">Datum</span></a>
-        <a href='index.php?filter=status'><span class="addBtn">status</span></a>
+        <a href='index.php'><span  style='margin-left:1em;margin-right:1em;' class="addBtn">Alles</span></a>
+        <a href='index.php?filter=checked' style='width:50px;'><button>Checked</button></a>
+        <a href='index.php?filter=unchecked' style='width:50px;'><button>Unchecked</button></a>
+        <a href='index.php?filter=status' style='width:50px;'><button>Sorteer status</button></a>
     </div>
 
     <ul id="myUL">
         <?php foreach($taken as $task){ 
 
+            if(isset($_GET['filter']) && $_GET['filter'] == 'checked'){
+                $query0072 = $conn->prepare('SELECT * FROM onderwerpen WHERE onderwerpen.taak_id = :taak AND onderwerpen.statuss = "check"');
+            } 
+            if(isset($_GET['filter']) && $_GET['filter'] == 'unchecked'){
+                $query0072 = $conn->prepare('SELECT * FROM onderwerpen WHERE onderwerpen.taak_id = :taak AND onderwerpen.statuss = ""');
+            } 
             if(isset($_GET['filter']) && $_GET['filter'] == 'status'){
-                $query0072 = $conn->prepare('SELECT * FROM onderwerpen WHERE onderwerpen.taak_id = :taak AND onderwerpen.statuss = "check" ORDER BY statuss DESC');
-            } else {
+                $query0072 = $conn->prepare('SELECT * FROM onderwerpen WHERE onderwerpen.taak_id = :taak ORDER BY statuss DESC');
+            }
+
+            if(!isset($_GET['filter'])){
                 $query0072 = $conn->prepare('SELECT * FROM onderwerpen WHERE onderwerpen.taak_id = :taak');
             }
             
@@ -43,12 +54,11 @@ $taken = $query0072->fetchAll();
             ?>
             <li>
 
-            <?php echo '<h3>'.$task['title']."<a href='edit.php?id={$task['id']}&t=t'> ✏️</a>".'</h3>' . '</a>' . 'Taken:'; foreach($onderwerpen as $ond){ echo '<div style='; if($ond['statuss'] == 'check'){ echo 'color:green'; } echo '>' . $ond['taak'] . " <a href='stat.php?id={$ond['id']} '> ✔</a> <a href='edit.php?id={$ond['id']} '> ✏️</a> <a href='deleteProc.php?id={$ond['id']} '> ✗</a>" . '</div>'; } ?>
+            <?php echo '<h3>'.$task['title']."<a href='edit.php?id={$task['id']}&t=t'> ✏️</a><a href='deleteProc.php?id={$task['id']}&t=t'> ✗</a>".'</h3>' . '</a>' . 'Taken:'; foreach($onderwerpen as $ond){ echo '<div style='; if($ond['statuss'] == 'check'){ echo 'color:green'; } echo '>' . $ond['taak'] . " <a href='stat.php?id={$ond['id']} '> ✔</a> <a href='edit.php?id={$ond['id']} '> ✏️</a> <a href='deleteProc.php?id={$ond['id']} '> ✗</a>" . '</div>'; } ?>
             <a href='create.php?id=<?php echo $task['id']; ?>&t=t'>
             <div>nieuwe taak toevoegen</div></li>
             </a>
             <a href='deleteProc.php?id=<?php echo $task['id']; ?>&t=t'>
-            <span class='close'>x</span></li>
             </a>
         <?php } ?>
     </ul>
